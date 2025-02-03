a786a2f0e2f35ff6109418a74ac4d827eebc274f
Robotics829 <hamilbra002@warren.k12.in.us>
2025-02-03 15:08:56 -0500

    BRANDON DID MORE STUFFF

--- src/main/java/frc/robot/mechanisms/wrist/WristTelemetry.java
+++ src/main/java/frc/robot/mechanisms/wrist/WristTelemetry.java
@@ -1,5 +1,11 @@
 package frc.robot.mechanisms.wrist;
 
+import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.units.AngularAccelerationUnit;
+import edu.wpi.first.units.AngularVelocityUnit;
+import edu.wpi.first.units.measure.Acceleration;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularAcceleration;
 import edu.wpi.first.units.measure.AngularVelocity;
@@ -7,18 +13,23 @@ import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.*;
 
 public class WristTelemetry {
     // TODO: all fields are private final
-    // TODO: create a NetworkTable called wristStateTable
-    // TODO: create an Angle called minAngle
-    // TODO: create an Angle called maxAngle
-    // TODO: create an AngularVelocity called maxVelocity
-    // TODO: create an AngularAcceleration called maxAcceleration
-    // TODO: create a DoublePublisher called timestamp;
-    // TODO: the rest are DoublePublishers
-    // TODO: for angle, angularVelocity, minAnglePublisher, maxAnglePublisher, maxVelocityPublisher, maxAccelerationPublisher,
+    private final NetworkTable wristStateTable;
+    private final Angle minAngle;
+    private final Angle maxAngle;
+    private final AngularVelocity maxVelocity;
+    private final AngularAcceleration maxAcceleration;
+    private final DoublePublisher timeStamp;
+    private final DoublePublisher angle;
+    private final DoublePublisher angularVelocity;
+    private final DoublePublisher minAnglePublisher;
+    private final DoublePublisher maxAnglePublisher;
+    private final DoublePublisher maxVelocityPublisher;
+    private final DoublePublisher maxAccelerationPublisher;
+
     private final Mechanism2d wristMechanism;
     private final MechanismLigament2d wristLigamentRight;
     private final MechanismLigament2d wristLigamentLeft;
@@ -29,13 +40,19 @@ public class WristTelemetry {
             Angle maxAngle,
             AngularVelocity maxVelocity,
             AngularAcceleration maxAcceleration) {
-        // TODO: set the fields for minAngle, maxAngle, maxVelocity, maxAcceleration.  Use the this keyword.
-        // TODO: this.minAngle = minAngle, etc.
-        // TODO: set this.wristStateTable to NetworkTableInstance.getDefault().getTable(name)
-        // TODO: set this.timestamp to armStateTable.getDoubleTopic("Timestamp").publish();
-        // TODO: set this.angle to armStateTable.getDoubleTopic("Angle").publish()
-        // TODO: set this.angularVelocity to armStateTable.getDoubleTopic("AngularVelocity").publish()
-        // TODO: repeat for minAnglePublisher, maxAnglePublisher, maxVelocityPublisher, maxAccelerationPublisher
+        this.minAngle = minAngle;
+        this.maxAngle = maxAngle;
+        this.maxVelocity = maxVelocity;
+        this.maxAcceleration = maxAcceleration;
+        this.wristStateTable = NetworkTableInstance.getDefault().getTable(name);
+        this.timeStamp = wristStateTable.getDoubleTopic("Angle").publish();
+        this.angle = wristStateTable.getDoubleTopic("Angle").publish();
+        this.angularVelocity = wristStateTable.getDoubleTopic("AngularVelocity").publish();
+        this.minAnglePublisher = wristStateTable.getDoubleTopic("MinAngle").publish();
+        this.maxAnglePublisher = wristStateTable.getDoubleTopic("MaxAngle").publish();
+        this.maxVelocityPublisher = wristStateTable.getDoubleTopic("MaxVelocity").publish();
+        this.maxAccelerationPublisher = wristStateTable.getDoubleTopic("MaxAcceleration").publish();
+
         wristMechanism = new Mechanism2d(1, 1);
         wristLigamentRight = wristMechanism
                 .getRoot("WristRoot", 0.5, 0.5)
@@ -47,13 +64,25 @@ public class WristTelemetry {
     }
 
     public void telemeterize(WristState state) {
-        // TODO: call minAnglePublisher's set method and pass in minAngle.in(Degrees)
-        // TODO: repeat for maxAnglePublisher using maxAngle
-        // TODO: repeat for maxVelocityPublisher using maxVelocity
-        // TODO: repeat for maxAccelerationPublisher using maxAcceleration
-        // TODO: repeat for timestamp using state.getTimeStamp.in(Seconds)
-        // TODO: repeat for angle using state.getPosition.in(Degrees)
-        // TODO: repeat for angularVelocity using state.getVelocity.in(DegreesPerSecond)
+        minAnglePublisher.set(minAngle.in(Degrees));
+        maxAnglePublisher.set(maxAngle.in(Degrees));
+        maxVelocityPublisher.set(maxVelocity.in(DegreesPerSecond));
+        maxAccelerationPublisher.set(maxAcceleration.in(DegreesPerSecondPerSecond));
+
+
+
+
+
+
+//        timeStamp.set(state.getTimeStamp().in(Seconds));
+
+
+
+
+
+
+        angle.set(state.getPosition().in(Degrees));
+        angularVelocity.set(state.getVelocity().in(DegreesPerSecond));
         wristLigamentRight.setAngle(state.getPosition().in(Degrees));
         wristLigamentLeft.setAngle(180.0 + state.getPosition().in(Degrees));
     }
