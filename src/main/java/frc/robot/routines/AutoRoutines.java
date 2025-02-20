77c5ee6f2966191abfb7655c99f8cdd865184f53
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-02-20 05:40:34 -0500

    End of Day 10

--- src/main/java/frc/robot/routines/AutoRoutines.java
+++ src/main/java/frc/robot/routines/AutoRoutines.java
@@ -7,7 +7,6 @@ import choreo.auto.AutoTrajectory;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
-import frc.robot.commandFactories.ManualFactories;
 import frc.robot.commandFactories.PickupFactories;
 import frc.robot.commandFactories.ScoringFactories;
 
@@ -32,6 +31,7 @@ public class AutoRoutines {
         final AutoRoutine routine = factory.newRoutine("The Love Boat Auto");
         final AutoTrajectory S3toG = routine.trajectory("S3toG");
         final AutoTrajectory GtoTopStation = routine.trajectory("GtoTopStation");
+
         routine.active().onTrue(
                 S3toG.resetOdometry()
                         .andThen(S3toG.cmd())
