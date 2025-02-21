e394fbde132110fc9139583211aeb2e6537cb444
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-02-20 19:44:52 -0500

    Save before Transit

--- src/main/java/frc/robot/routines/AutoRoutines.java
+++ src/main/java/frc/robot/routines/AutoRoutines.java
@@ -5,12 +5,16 @@ import choreo.auto.AutoFactory;
 import choreo.auto.AutoRoutine;
 import choreo.auto.AutoTrajectory;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import frc.robot.commandFactories.PickupFactories;
 import frc.robot.commandFactories.ScoringFactories;
 
+import static edu.wpi.first.wpilibj2.command.Commands.*;
+
 public class AutoRoutines {
+
+
+
     private final AutoFactory factory;
 
     public AutoRoutines(
@@ -26,15 +30,17 @@ public class AutoRoutines {
     }
 
     private AutoRoutine theNoobSpot() {
-        final AutoRoutine routine = factory.newRoutine("The Noob Spot");
-        final AutoTrajectory S2toG = routine.trajectory("S2-to-G");
+        AutoRoutine routine = factory.newRoutine("The Noob Spot");
+        AutoTrajectory S2toG = routine.trajectory("S2-to-G");
 
         routine.active().onTrue(
-                S2toG.resetOdometry()
-                        .andThen(S2toG.cmd())
-                        .andThen(Commands.print("SCORE L4"))
-                        .andThen(Commands.waitSeconds(2))
-                        .withName("The Noob Spot"));
+                sequence(
+                        S2toG.resetOdometry(),
+                        S2toG.cmd()));
+
+
+
         return routine;
+
     }
 }
