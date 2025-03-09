6aa837645792ecfd480a06a0f8d72b857339165b
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-03-09 16:49:34 -0400

    Saving

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,11 +1,7 @@
 package frc.robot;
 
 import com.ctre.phoenix6.CANBus;
-import edu.wpi.first.units.AngleUnit;
-import edu.wpi.first.units.DistanceUnit;
 import edu.wpi.first.wpilibj.RobotController;
-
-import static edu.wpi.first.units.Units.*;
 
 public class Constants {
 
@@ -14,11 +10,6 @@ public class Constants {
     }
 
     public static final double controllerDeadband = 0.1;
-    public static final DistanceUnit Corbins = derive(Feet).aggregate(6).named("Corbin").symbol("cb").make();
-    private static final DistanceUnit Corbin = Corbins;
-    private static final DistanceUnit Adams = Corbins;
-    private static final DistanceUnit Adam = Adams;
-    private static final AngleUnit Angel = derive(Degrees).aggregate(90).named("Angel").symbol("al").make();
     public static final CANBus rio = new CANBus("rio");
     public static final CANBus canivore = new CANBus("canivore", "./logs/example.hoot");
     public static final String robotComments = RobotController.getComments();
